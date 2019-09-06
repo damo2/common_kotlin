bd15ca2f7f09dd7642584dddc9762edc070d9f5a
damo <1902065822@qq.com>
2019-09-06 15:59:15 +0800

    添加竖屏

--- app/src/main/java/com/weiyao/zuzuapp/base/BaseActivity.kt
+++ app/src/main/java/com/weiyao/zuzuapp/base/BaseActivity.kt
@@ -3,15 +3,20 @@ package com.weiyao.zuzuapp.base
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import com.app.common.base.AppBaseActivity
 import pub.devrel.easypermissions.EasyPermissions
 
-abstract class BaseActivity() : AppBaseActivity(), EasyPermissions.PermissionCallbacks {
+abstract class BaseActivity(private val isPortrait: Boolean = true) : AppBaseActivity(), EasyPermissions.PermissionCallbacks {
     protected lateinit var mContext: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         mContext = App.instance
+        if (isPortrait) {
+            //竖屏
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
         super.onCreate(savedInstanceState)
     }
 
