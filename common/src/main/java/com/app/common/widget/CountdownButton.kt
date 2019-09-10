e0a5633d5a0c13c69800393a3f303f9a9e991e9a
damo <1902065822@qq.com>
2019-09-10 17:18:35 +0800

    优化倒计时按钮

--- common/src/main/java/com/app/common/widget/CountdownButton.kt
+++ common/src/main/java/com/app/common/widget/CountdownButton.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import com.app.common.CommonConst
 import com.app.common.R
 import com.app.common.save.Preference
-import com.app.common.save.PreferenceNonNull
 
 /**
  * 点击倒计时。
@@ -27,7 +26,9 @@ class CountdownButton(context: Context, attrs: AttributeSet) : Button(context, a
     //是否退出依然倒计时，默认false
     private val isExitTiming: Boolean
     //倒计时开始时间
-    private var countdownTime: Long? by Preference(context, "countdown_time_$id", 0, CommonConst.PREFERENCE_FILENAME)
+    private var countdownTime: Long? by Preference(context, "countdown_time_$id", 0L, CommonConst.PREFERENCE_FILENAME)
+
+    private var onTimeCallBack: ((time: Long, isFinish: Boolean) -> Unit)? = null
 
     companion object {
         const val DOWNFORMAT_DEFAULT = "%s 秒"
@@ -60,6 +61,7 @@ class CountdownButton(context: Context, attrs: AttributeSet) : Button(context, a
             if (time > 0) {
                 val timeCount = TimeCount(time, interval.toLong())
                 timeCount.start()
+                onTimeCallBack?.invoke(time, false)
             }
         }
     }
@@ -68,9 +70,14 @@ class CountdownButton(context: Context, attrs: AttributeSet) : Button(context, a
     fun startCountdown() {
         val time = TimeCount(total.toLong(), interval.toLong())
         time.start()
+        onTimeCallBack?.invoke(total.toLong(), false)
         if (isExitTiming) {
             countdownTime = System.currentTimeMillis()
         }
+    }
+
+    fun setOnTimeCallback(onTimeCallBack: ((time: Long, isFinish: Boolean) -> Unit)?) {
+        this.onTimeCallBack = onTimeCallBack
     }
 
     //millisInFuture 总时长, countDownInterval 时间间隔
@@ -82,13 +89,15 @@ class CountdownButton(context: Context, attrs: AttributeSet) : Button(context, a
             if (isExitTiming) {
                 countdownTime = 0
             }
+            onTimeCallBack?.invoke(0, true)
         }
 
         override fun onTick(millisUntilFinished: Long) {
             isEnabled = false
             val count = millisUntilFinished / countDownInterval
+            onTimeCallBack?.invoke(count, false)
             if (count > 0) {
-                text = String.format(downFormat, count.toString())
+                text = String.format(downFormat, "$count")
             }
         }
     }
